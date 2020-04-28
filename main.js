059c3a112f2d4abb9d1b9191bec3eeb3f8672ed0
Rimantas <belauzas@yahoo.com>
2020-04-28 11:35:14 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// localStorage
+
+const pazymiai = [10, 8, 6, 2, 7, 5, 9, 10];
+
+// map, sort, filter, reduce
+
+const dvygubiPazymiai = pazymiai.map( a => a * 2 );
+
+console.log(pazymiai);
+console.log(dvygubiPazymiai);
+
+const liekanos = pazymiai.map( g => g % 2 );
+console.log(liekanos);
+
+const pazymiaiMinMax = pazymiai.sort( (a, b) => a - b );
+console.log(pazymiaiMinMax);
+
+const pazymiaiMore5 = pazymiai.filter( a => a > 5 );
+console.log(pazymiaiMore5);
+
+const pazymiaiLess7 = pazymiai.filter( a => a < 7 );
+console.log(pazymiaiLess7);
+
+const skaiciai = [1, 2, 3, 4];
+const pazymiaiSum = skaiciai.reduce( (total, a) => total - a );
+console.log(pazymiaiSum);
+
+let objs = [{a:10}, {a:8}, {a:2}, {a:6}, {a:4}];
+const sumObjs = objs.reduce( (total, obj) => total + obj.a, 0 );
+console.log(sumObjs);
